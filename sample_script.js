ecf466c96266b14f4844672d5208923a6b2aa1cf
Ali <a.ghaffari1380@gmail.com>
2020-10-02 14:46:20 +0330

    First commit

---
+++ sample_script.js
@@ -0,0 +1,300 @@
+var a =1;
+
+var langStat = 0;
+
+var pauseStat = 1;
+
+var places = {
+
+    place1 : {name:"گنبد سلطانیه",
+        name_en:"Dome of Soltanieh",
+        explain:" گنبد سلطانیه آرامگاه اُلجایتو است که بین سال‌های ۱۳۰۲ تا ۱۳۱۲ میلادی به دستور او در شهر سلطانیه (پایتخت ایلخانیان) ساخته شد و از آثار مهم معماری ایرانی و اسلامی به ‌شمار می‌رود. گنبد سلطانیه در فهرست آثار میراث جهانی به ثبت رسیده ‌است. گنبد این بنا سومین گنبد بزرگ جهان بعد از گنبد کلیسای سانتا ماریا دل فیوره، و گنبد مسجد ایاصوفیه است.<br>این بنا شامل ۸ ایوان و ۸ مناره است که گفته می‌شود از ۸ در بهشت (ابواب البر یا درهای نیکی) الگو گرفته‌است. بنا دربرگیرنده دو دوره تزئینات می‌باشد. دور اول تزئینات آجر و کاشی بوده که به واسطه آجر و کاشی آیاتی از قرآن و نام الله و محمد و علی و نام سلطان نوشته شده‌است اما پس از مدتی به دلیلی ناشناخته سلطان محمد خدابنده دستور پوشانیدن تزئینات کاشی با اندود گچ را می‌دهد.",
+        explain_en:"The dome of Soltanieh is the tomb of Oljaito, which was built between 1302 and 1312 AD by his order in the city of Soltanieh (the capital of the Ilkhanids) and is one of the most important works of Iranian and Islamic architecture. The dome of Soltanieh is registered in the list of world heritage sites. The dome of this building is the third largest dome in the world after the dome of the church of Santa Maria del Fiore, and the dome of the Hagia Sophia.<br> The building includes two periods of decoration. The first round was the decoration of bricks and tiles, through which verses from the Qur'an, the names of Allah, Muhammad, Ali, and the name of the Sultan were written, but after some time, for some unknown reason, Sultan Mohammad Khodabandeh ordered the tiles to be covered with plaster.\n",
+        cordinate:[36.433818,48.795885]},
+
+    place2 : {name:"غار کتله خور",
+        name_en:"Katalekhor Cave",
+        explain:"غار کتله‌خور غاری است خشکی -آبی که در استان زنجان، شهرستان خدابنده و در ۸۰ کیلومتری جنوب قیدار و ۱۷۳ کیلومتری شمال همدان و ۴۱۰ کیلومتری تهران و ۵ کیلومتری شهر گرماب واقع است. در بعضی مناطق غربی ایران به کوه کم ارتفاع \"کَتَله\" می‌گویند و احتمالاً وجه تسمیه این غار نیز همین است که در یکی از این کوه‌ها قرار گرفته‌است و خورشید از پشت آن طلوع می‌کند و به جای کتله خورشید به آن کتله خور می‌گویند.<br>غار کتله‌خور ازلحاظ کیفیت بلورها و قندیل‌ها، زیبایی و تعدد طبقات اولین غار آهکی جهان شناخته شده‌است که توسط سرمایه‌گذاری بخش خصوصی و هزینه شخصی به نام مهندس عالی که مهندس عمران فرمانداری زنجان بود در طول پنج سال آماده بهره‌برداری گشت. از بهره‌برداری اولیه این غار چیزی حدود ۱۵ سال می‌گذرد که البته هنوز هم کاوش و فعالیت برای آماده‌سازی سایر قسمت‌های غار برای بازدید ادامه دارد.",
+        explain_en: "Katlekhor Cave is a land-water cave located in Zanjan province, Khodabandeh city, 80 km south of Qeydar, 173 km north of Hamedan, 410 km from Tehran and 5 km from Garmab city. In some western regions of Iran, the low mountain is called \"Katleh\" and probably the name of this cave is that it is located in one of these mountains and the sun rises behind it and instead of the sun is called \"Katlekhor\".<br> Katlekhor Cave is known in the world for its quality of crystals and chandeliers, beauty and multiplicity of floors. It has been about 15 years since the initial operation of this cave, but of course, excavations and activities are still going on to prepare other parts of the cave for visiting.",
+        cordinate:[35.835681,48.162313]},
+
+    place3 : {name:"عمارت ذوالفقاری",
+        name_en:"Zolfaghari Mansion",
+        explain:"خانه ذوالفقاری یا عمارت ذوالفقاری مربوط به دوره قاجار است و در زنجان، خیابان طالقانی واقع شده و این اثر در تاریخ ۲۵ اسفند ۱۳۷۵ با شمارهٔ ثبت ۱۸۵۲ به‌عنوان یکی از آثار ملی ایران به ثبت رسیده‌است.<br>این بنا در یکی از محلات قدیمی شهر زنجان به نام موسوم به محله «دالان آلتی» در ضلع شمالی سبزه میدان و مسجد جامع قرار دارد. این بنا ساختمان مسکونی و اداری حکمران زنجان بوده و از مجموعه بناهای اندرونی و بیرونی و بخش‌های مختلفی تشکیل یافته بود. از این مجموعه در حال حاضر فقط عمارت بیرونی باقی‌مانده که به نام «عمارت محمود خان ذوالفقاری» معروف است.<br>در حال حاضر این عمارت تبدیل به موزهٔ سه تن از مردان نمکی شده‌است. بعد از گشایش این موزه سه جسد از چهار جسد برای بازدید عموم از موزه رختشویخانه زنجان به این مکان منتقل شدند.",
+        explain_en:"Zolfaghari House or Zolfaghari Mansion belongs to the Qajar period and is located in Taleghani Street in Zanjan. This work was registered on March 16, 1996 with the registration number 1852 as one of the national monuments of Iran.<br> This building is located in one of the old neighborhoods of Zanjan city called \"Dalan Alti\" neighborhood on the north side of Sabzeh Square and the Grand Mosque. This building was the residential and office building of the ruler of Zanjan and consisted of a set of internal and external buildings and different sections. From this complex, only the outer mansion remains, which is known as \"Mahmoud Khan Zolfaghari mansion\".<br> The mansion has now been turned into a museum of three salt men. After the opening of this museum, three of the four bodies were transferred to this place for public viewing of the Zanjan Laundry Museum.",
+        cordinate:[36.670900,48.480668]},
+
+    place4 : {name:"مسجد جامع زنجان",
+        name_en:"Zanjan Grand Mosque",
+        explain:"مسجد و مدرسه جامع زنجان معروف به مسجد سید و مسجد جامع سید (سلطانی) مربوط به اوایل دوره قاجار است و در زنجان، جنوب سبزه میدان واقع شده و این اثر با نام «مسجد جامع سید (سلطانی)» در تاریخ ۱۰ اردیبهشت ۱۳۴۲ با شمارهٔ ثبت ۱۰۵۶ به‌عنوان یکی از آثار ملی ایران به ثبت رسیده‌است.<br>مسجد و مدرسه جامع زنجان در قرن سیزدهم هجری قمری (سال ۱۲۴۲) در دوره قاجاریه توسط یکی از پسران فتحعلی شاه قاجار به نام عبدالله میرزا دارا که مرید آیت‌الله سید محمد مجتهد سردانی بود ساخته شد تا مرکز تدریس، اقامه نماز و مراجعات مردمی این مجتهد زنجانی باشد. خاندان حسینی زنجانی از آن زمان تا کنون تولیت مسجد را به عهده دارند. این مسجد که در قلب بافت قدیمی شهر و در مکان مناسبی از نظر دسترسی قرار گرفته‌است، از سمت باختر با بازار قیصریه، از سمت خاور با کوچه مسجد سید و از سوی شمال با خیابان امام خمینی و سبزه میدان درارتباط است. این مسجد در دوره زندگانی بانی آن به نام‌های مسجد دارا، مسجد سید، مسجد سلطانی و مسجد جمعه نیز خوانده می‌شده‌است.",
+        explain_en:"Zanjan Mosque and Comprehensive School, known as Seyed Mosque and Seyed (Soltani) Mosque, belong to the early Qajar period and are located in Zanjan, south of Sabzeh Maidan. Registration number 1056 has been registered as one of the national monuments of Iran.<br> Zanjan Mosque and Comprehensive School was built in the 13th century AH (1242) during the Qajar period by one of the sons of Fath Ali Shah Qajar named Abdullah Mirza Dara, who was a disciple of Ayatollah Seyyed Mohammad Mujtahid Sardani, to be the center of teaching, prayer and public visits of this Mujtahid. Be from Zanjani. The Hosseini Zanjani family has been in charge of the mosque ever since. This mosque, which is located in the heart of the old part of the city and in a suitable place in terms of access, is connected with Qaisaria Bazaar from the west, Seyed Mosque alley from the east and Imam Khomeini Street and Sabzeh Maidan from the north. This mosque was also called Dara Mosque, Seyed Mosque, Soltani Mosque and Juma Mosque during its founder.",
+        cordinate:[36.668246,48.479663]},
+
+    place5 : {name:"رختشویخانه",
+        name_en:"Zanjan Laundry",
+        explain:"بنای رخت‌شوی‌خانه یا مجموعهٔ تاریخی رخت‌شوی‌خانه در قلب بافت تاریخی شهر زنجان که منطبق بر حصار قدیمی شهر می‌باشد، در یک منطقهٔ پرتراکم مسکونی احداث شده‌است. مردم شهر لباس و رخت خود را در آن محل می‌شسته‌اند. چنین کاربری برای یک بنای عمومی در جهان مشابه ندارد یا کم‌نظیر است. این بنا در حال حاضر تعمیر و مرمت شده و به عنوان موزهٔ مردم‌شناسی مورد استفاده قرار می‌گیرد و مردم می‌توانند از آن بازدید کنند. این بنا در سال‌های گذشته به جهت وجود هوای سرد در زمستان‌ها برای بانوان زنجانی ساخته شده‌است.<br>از نظر تشابه کاربری چنین بنایی در هیچ قسمتی از ایران به این شکل دیده نشده ولی از نظر معماری با مسجد عباسقلی خان و دروازه ارگ قابل مقایسه‌است.<br>",
+        explain_en:"The building of the laundry or the historical complex of the laundry has been built in the heart of the historical context of Zanjan, which corresponds to the old fence of the city, in a densely populated area. The people of the city were washing their clothes there. Such a use is unparalleled or rare for a public building in the world. The building is currently being renovated and used as an anthropological museum, and people can visit it. This building was built for Zanjani women in recent years due to the cold weather in winter.<br> In terms of similarity of use, such a building has not been seen in this way in any part of Iran, but in terms of architecture, it is comparable to Abbas Gholi Khan Mosque and Arg Gate.",
+        cordinate:[36.671164,48.485008]},
+
+    place6 : {name:"قلعه‌ بهستان",
+        name_en:"Behestan Castle",
+        explain:"قلعه بهستان یا کهن دژ در شهرستان ماه‌نشان در استان زنجان و در نزدیکی روستای بهستان و در کنار رود قزل اوزن جای گرفته‌است. دیرینگی این دژ به دوره مادها بازمی‌گردد. این دژ در تاریخ ۱۳۵۶/۰۷/۱۱ و با شماره ثبت ملی ۱۴۵۸ به نام قلعه بهستان در فهرست آثار ملی کشور به ثبت رسیده‌است. قلعه‌ای منحصر به فرد که متأسفانه به علت عدم توجه مسئولان میراث فرهنگی کشور و بخصوص استان در آستانه تخریب است. توجه به ساخت دیوارهای حائل در کنار رودخانه قزل‌اوزن برای حفاظت از قلعه بسیار ضروری است.<br>نمای بیرونی قلعه به شکل دیواره‌ای از دودکش‌های به هم پیوسته و تعدادی دودکش جدا است که حفره‌های پایین قلعه، ظاهری مرموز بدان بخشیده است. برخی از مردم محلی این آثار را به دلیل شکل عجیبشان «دودکش جن» می‌نامند و برخی نیز این قلعه را تخت دیو می‌نامند زیرا معتقد بودند دیوها روی تخته سنگ‌های مسطح بالای ستون‌ها استراحت می‌کنند.",
+        explain_en:"Behestan Castle or Kohan Dej is located in Mahneshan city in Zanjan province, near Behestan village and next to Ghezel Ozan river. The longevity of this fortress dates back to the Median period. This fort has been registered in the list of national monuments of the country on 11/07/1357 with the national registration number 1458 called Qaleh Behestan. A unique castle that is unfortunately on the verge of destruction due to the lack of attention of the cultural heritage officials of the country, especially the province. It is very important to pay attention to the construction of retaining walls along the Ghezel Ouzen River to protect the castle.<br> The exterior of the castle is in the form of a wall of interconnected chimneys and a number of separate chimneys, which have been given a mysterious appearance by the cavities at the bottom of the castle. Some locals call these monuments \"demon chimneys\" because of their strange shape, and others call the castle the Demon Bed because they believed that demons rested on flat boulders above the pillars.",
+        cordinate:[36.660538,47.736571]}
+
+}
+
+document.getElementById("title").innerHTML = places["place"+a].name;
+document.getElementById("explain").innerHTML = places["place"+a].explain;
+document.getElementById("mainImg").src = 'images/img'+a+'.jpg';
+document.getElementById("counter").innerHTML = a + " / 6";
+
+var mapOptions = {
+    center: [36.676162, 48.499972],
+    zoom: 12,
+}
+
+var interval = setInterval(slide,16000);
+
+
+var map = new L.map('mapDiv',mapOptions);
+var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+map.addLayer(layer);
+
+var marker = L.marker(places["place"+a].cordinate).addTo(map);
+map.panTo(places["place"+a].cordinate);
+map.setZoom(15);
+
+function panTo(){
+
+    var marker = L.marker([36.685966, 48.399060]).addTo(map);
+
+    map.panTo([36.685966, 48.399060])
+
+    // map.panTo([36.652887, 48.528250]); //basij_square
+    // setTimeout(map.setZoom(15),5000);
+    setTimeout(() => { map.setZoom(12); }, 400);
+    // map.setZoom(15);
+}
+
+function nextClick(){
+
+    slide();
+    document.getElementById("nextBtn").style.pointerEvents = "none";
+
+}
+
+function prevClick(){
+
+    if (a>1){
+        document.getElementById("prevBtn").style.pointerEvents = "none";
+        a = a - 2;
+        slide();
+    }else if (a===1){
+        document.getElementById("prevBtn").style.pointerEvents = "none";
+        a=5;
+        slide();
+    }
+
+}
+
+
+function slide(){
+
+    if (pauseStat===1){
+        clearInterval(interval);
+        interval = setInterval(slide,16000);
+    }
+
+    photoAnim();
+
+    textAnim();
+
+    map.removeLayer(marker);
+
+    if (a==6){
+        a=1;
+    }else {
+        a++;
+    }
+    map.setZoom(12);
+
+    setTimeout(() => { document.getElementById("nextBtn").style.pointerEvents = "initial"; }, 3000);
+    setTimeout(() => { document.getElementById("prevBtn").style.pointerEvents = "initial"; }, 3000);
+
+    marker = L.marker(places["place"+a].cordinate).addTo(map);
+
+
+    document.getElementById("counter").innerHTML = a + " / 6";
+
+    setTimeout(() => { map.panTo(places["place"+a].cordinate); }, 1000);
+    setTimeout(() => { map.setZoom(15); }, 2000);
+    setTimeout(() => { map.setZoom(17); }, 4500);
+
+}
+
+var imgOp = 1;
+var imgStat = 0;
+
+function photoAnim(){
+
+    var interval1 = setInterval(changePhoto,30);
+
+    function changePhoto(){
+        if (imgStat===0){
+
+            if (imgOp<0.2){
+
+                document.getElementById("mainImg").src = "images/img"+a+".jpg";
+                imgStat=1;
+
+            }else {
+                imgOp = imgOp - 0.1;
+                document.getElementById("mainImg").style.opacity = imgOp +"";
+            }
+
+        }else {
+
+            if (imgOp>0.8){
+
+                document.getElementById("mainImg").style.opacity = 1 +"";
+                clearInterval(interval1);
+                imgOp = 1;
+                imgStat=0;
+
+            }else {
+                imgOp = imgOp + 0.1;
+                document.getElementById("mainImg").style.opacity = imgOp +"";
+            }
+
+
+        }
+    }
+}
+
+var textStat = 0;
+var textOP = 1;
+
+
+function textAnim(){
+
+    var interval2 = setInterval(changeText,45);
+
+    function changeText(){
+
+        if (textStat===0){
+
+            if (textOP<0.2){
+
+                if (langStat===0){
+
+                    document.getElementById("title").innerHTML = places["place"+a].name;
+                    document.getElementById("explain").innerHTML = places["place"+a].explain;
+
+                }else {
+
+                    document.getElementById("title").innerHTML = places["place"+a].name_en;
+                    document.getElementById("explain").innerHTML = places["place"+a].explain_en;
+
+                }
+                textStat=1;
+            }else {
+                textOP = textOP - 0.1 ;
+                document.getElementById("title").style.opacity = textOP +"";
+                document.getElementById("explain").style.opacity = textOP +"";
+                document.getElementById("title").style.marginRight = textOP*(7) +"%";
+                document.getElementById("explain").style.lineHeight = (textOP*2.2)+"vw";
+            }
+
+        }else {
+
+            if (textOP>0.8){
+                document.getElementById("title").style.opacity = 1 +"";
+                document.getElementById("explain").style.opacity = 1 +"";
+                document.getElementById("title").style.marginRight = 6 +"%";
+                document.getElementById("explain").style.lineHeight = 2.2+"vw";
+                clearInterval(interval2);
+                textStat=0;
+                textOP = 1;
+            }else {
+                textOP = textOP + 0.1 ;
+                document.getElementById("title").style.opacity = textOP +"";
+                document.getElementById("explain").style.opacity = textOP +"";
+                document.getElementById("title").style.marginRight = textOP*(7) +"%";
+                document.getElementById("explain").style.lineHeight = (textOP*2.2)+"vw";
+            }
+        }
+
+    }
+
+}
+
+var themeStat = 1;
+
+function changeTheme() {
+
+    if (themeStat===1){
+        setThemeColors("#f0f0f0","#212121")
+        themeStat = 0;
+    }else if (themeStat===0){
+        setThemeColors("#1d1d1d","#f4f4f4")
+        themeStat = 1;
+    }
+}
+
+function setThemeColors(txtColor,backColor){
+
+    document.getElementById("mainDiv").style.background = backColor+"";
+    document.getElementById("title").style.color = txtColor+""
+    document.getElementById("explain").style.color = txtColor+""
+    document.getElementById("counter").style.color = txtColor+""
+
+}
+
+function changeLang(){
+
+    if (langStat===0){
+
+        document.getElementById("mainDiv").style.transform = "scaleX(-1)";
+        document.getElementById("photoCont").style.transform = "scaleX(-1)";
+        document.getElementById("textCont").style.transform = "scaleX(-1)";
+        document.getElementById("title").style.direction = "ltr"
+        document.getElementById("explain").style.direction = "ltr"
+        document.getElementById("explain").style.fontSize = "1vw"
+
+        document.getElementById("title").innerHTML = places["place"+a].name_en;
+        document.getElementById("explain").innerHTML = places["place"+a].explain_en;
+
+        langStat = 1;
+    }else {
+
+        document.getElementById("mainDiv").style.transform = "scaleX(1)";
+        document.getElementById("photoCont").style.transform = "scaleX(1)";
+        document.getElementById("textCont").style.transform = "scaleX(1)";
+        document.getElementById("title").style.direction = "rtl"
+        document.getElementById("explain").style.direction = "rtl"
+        document.getElementById("explain").style.fontSize = "1.4vw"
+
+        document.getElementById("title").innerHTML = places["place"+a].name;
+        document.getElementById("explain").innerHTML = places["place"+a].explain;
+
+        langStat = 0;
+    }
+
+
+}
+
+
+function pausePlay(){
+    if (pauseStat===0){
+        interval = setInterval(slide,16000);
+        document.getElementById("imgPause").src = "images/pause.png"
+        pauseStat = 1;
+    }else {
+        clearInterval(interval)
+        document.getElementById("imgPause").src = "images/play.png"
+        pauseStat = 0;
+    }
+
+}
